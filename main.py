9ce4fec316013e5399eda33d2d07b8191e397379
ShotaSasaki-HU <invisiblefrontline@gmail.com>
2026-04-06 04:08:49 +0900

    feat: クロスプラットフォームなディスプレイ管理

--- main.py
+++ main.py
@@ -74,7 +74,19 @@ class SpaceDebrisApp:
     """
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH_INIT, SCREEN_HEIGHT_INIT), pygame.RESIZABLE)
+
+        # デフォルトはフラグなし（WASM環境など向け）
+        display_flags = 0
+        # 'emscripten'はブラウザ(WASM)環境を示す文字列
+        # それ以外の環境（ローカルPC）でのみ，RESIZABLEフラグを付与．
+        if sys.platform != "emscripten":
+            display_flags = pygame.RESIZABLE
+            
+        self.screen = pygame.display.set_mode(
+            (SCREEN_WIDTH_INIT, SCREEN_HEIGHT_INIT), 
+            display_flags
+        )
+
         pygame.display.set_caption("Space Debris Cleaner")
         self.clock = pygame.time.Clock()
         self.running = True
